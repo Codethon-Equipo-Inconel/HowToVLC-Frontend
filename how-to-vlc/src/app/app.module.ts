a5c5c6d1dd16839431ea7c9ebda641f82fdf716f
Ammiel Rarak <ammielary@gmail.com>
2024-03-25 19:29:46 +0100

    se crearon los componentes header y recursos

--- how-to-vlc/src/app/app.module.ts
+++ how-to-vlc/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { RecursosComponent } from './pages/recursos/recursos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecursosComponent
   ],
   imports: [
     BrowserModule,
