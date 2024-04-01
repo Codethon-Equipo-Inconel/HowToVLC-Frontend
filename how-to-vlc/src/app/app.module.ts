c50af69cadce81cf1f4e28e8950556cda3032341
WhatAnAngel <angelilai.al@hotmail.com>
2024-04-01 17:27:56 +0200

    He hecho el formulario de registro, esta en components/login :)

--- how-to-vlc/src/app/app.module.ts
+++ how-to-vlc/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { RecursosComponent } from './pages/recursos/recursos.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     RecursosComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
