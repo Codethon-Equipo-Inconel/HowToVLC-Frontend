b7b08bb621f13ad7a72e4a4ff5a68cbd95e68352
AmmielRarak <ammielary@gmail.com>
2024-04-24 22:00:33 +0200

    se añadio claves para el acceso a la BD de supabase

--- how-to-vlc/src/environments/environment.development.ts
+++ how-to-vlc/src/environments/environment.development.ts
@@ -1,4 +1,7 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:8080'
+    supabase: {
+        url: 'https://pposjniuycymjvkstujt.supabase.co',
+        key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBwb3Nqbml1eWN5bWp2a3N0dWp0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTM5Nzk0MzksImV4cCI6MjAyOTU1NTQzOX0.SnLsF2QJOZXWDnljDsbFRRCLS94kwosDRBZ1RgegQbY'
+    }
 };
