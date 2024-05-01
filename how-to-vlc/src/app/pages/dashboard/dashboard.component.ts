8dbd49757706199bae61d142c758ef39d0b64750
WhatAnAngel <angelilai.al@hotmail.com>
2024-05-01 17:32:45 +0200

    he conseguido linkear la galeria al dashboard

--- how-to-vlc/src/app/pages/dashboard/dashboard.component.ts
+++ how-to-vlc/src/app/pages/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { GalleryComponent } from '../eventos/componentes/gallery/gallery.component';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent {
+
+  fotosEventos = new GalleryComponent;
 
 }
